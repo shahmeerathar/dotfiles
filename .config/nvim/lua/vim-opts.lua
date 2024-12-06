b3b9ab43f335fe020c96f19333f2aa824c6c80db
Shahmeer Athar <shahmeer.athar@gmail.com>
2024-12-06 15:44:30 -0800

    Keymap for file explorer

--- .config/nvim/lua/vim-opts.lua
+++ .config/nvim/lua/vim-opts.lua
@@ -15,3 +15,4 @@ vim.opt.scrolloff = 15
 vim.opt.signcolumn = 'yes'
 
 vim.keymap.set('n', '<leader><leader>', ':noh<CR>', { silent = true })
+vim.keymap.set('n', '<leader>ft', ':Ex<CR>', { silent = true })
