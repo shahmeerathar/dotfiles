4e1dfb7d5329d5ff1a85748e4653d0f36d2d4159
Shahmeer Athar <shahmeer.athar@gmail.com>
2024-12-07 01:03:41 -0500

    nvim: luasnip

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -3,13 +3,16 @@ return {
         "hrsh7th/nvim-cmp",
         config = function()
             local cmp = require('cmp')
+            require("luasnip.loaders.from_vscode").lazy_load()
+
             cmp.setup({
                 sources = {
                     { name = 'nvim_lsp' },
+                    { name = 'luasnip' }
                 },
                 snippet = {
                     expand = function(args)
-                        vim.snippet.expand(args.body)
+                        require 'luasnip'.lsp_expand(args.body)
                     end,
                 },
                 mapping = cmp.mapping.preset.insert({
@@ -18,15 +21,57 @@ return {
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                    ['<Tab>'] = cmp.mapping(function(fallback) -- Super tab
+                        local luasnip = require('luasnip')
+                        local col = vim.fn.col('.') - 1
+
+                        if cmp.visible() then
+                            cmp.select_next_item({ behavior = 'select' })
+                        elseif luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                            fallback()
+                        else
+                            cmp.complete()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback) -- Super shift tab
+                        local luasnip = require('luasnip')
+
+                        if cmp.visible() then
+                            cmp.select_prev_item({ behavior = 'select' })
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
                 }),
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered()
-                }
+                },
+                preselect = 'item',
+                completion = {
+                    completeopt = 'menu,menuone,noinsert'
+                },
+                formatting = {
+                    format = function(entry, vim_item)
+                        vim_item.menu = entry.source.name
+                        return vim_item
+                    end,
+                },
             })
         end
     },
     {
         "hrsh7th/cmp-nvim-lsp",
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
     }
 }
