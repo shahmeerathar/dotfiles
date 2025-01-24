f85341039bebe6f511c29f463f9cda23d43dd091
Shahmeer Athar <shahmeer.athar@gmail.com>
2025-01-24 13:10:15 -0800

    Telescope grep string mappings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -12,5 +12,14 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "Telescope view open buffers" })
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set('n', '<leader>fw', function()
+            builtin.grep_string({ search = vim.fn.expand('<cword>') })
+        end, { desc = "Telescope grep word" })
+        vim.keymap.set('n', '<leader>fW', function()
+            builtin.grep_string({ search = vim.fn.expand('<cWORD>') })
+        end, { desc = "Telescope grep WORD" })
+        vim.keymap.set('n', '<leader>fs', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end, { desc = "Telescope grep string" })
     end
 }
