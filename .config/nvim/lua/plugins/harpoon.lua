8bece3ea4001667ebad34f972ecda805c036969a
Shahmeer Athar <shahmeer.athar@gmail.com>
2025-08-28 07:03:02 -0700

    Updated harpoon keybinds

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,13 +9,16 @@ return {
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "Harpoon add" })
         vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
             { desc = "Harpoon list" })
-        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end, { desc = "Harpoon previous" })
-        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end, { desc = "Harpoon next" })
+        vim.keymap.set("n", "<C-M-p>", function() harpoon:list():prev() end, { desc = "Harpoon previous" })
+        vim.keymap.set("n", "<C-M-n>", function() harpoon:list():next() end, { desc = "Harpoon next" })
 
         for _, idx in ipairs { 1, 2, 3, 4, 5, 6, 7, 8, 9 } do
             vim.keymap.set("n", string.format("<leader>%d", idx), function()
                 harpoon:list():select(idx)
             end, { desc = string.format("Harpoon jump to %d", idx) })
+            vim.keymap.set("n", string.format("<leader>v%d", idx), function()
+                harpoon:list():select(idx, { vsplit = true })
+            end, { desc = string.format("Harpoon jump to %d in new vertical split", idx) })
         end
 
         harpoon:extend({
