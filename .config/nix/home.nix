74a8f9dbede801baed83b2295cf023ee9b87e5f7
Shahmeer Athar <shahmeer.athar@gmail.com>
2025-09-09 14:48:40 -0400

    Nix git

--- .config/nix/home.nix
+++ .config/nix/home.nix
@@ -22,6 +22,17 @@ in {
 
   programs.home-manager.enable = true;
 
+  programs.git = {
+    enable = true;
+    userEmail = "shahmeer.athar@gmail.com";
+    userName = "Shahmeer Athar";
+    extraConfig = {
+      commit.gpgsign = true;
+      gpg.format = "ssh";
+      user.signingkey = "~/.ssh/id_ed25519.pub";
+    };
+  };
+
   programs.zsh = {
     enable = true;
     oh-my-zsh = {
