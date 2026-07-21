b495a9dcdb191e96fe6be57f34b7594f49d073a1
Shahmeer Athar <shahmeer.athar@gmail.com>
2026-07-21 14:27:39 -0700

    Clangd indexing progress bar

--- configs/nvim/lua/plugins/lualine.lua
+++ configs/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,31 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local lualine = require('lualine')
+
+        local clangd_index_percent = nil
+
+        vim.api.nvim_create_autocmd('LspProgress', {
+            callback = function(ev)
+                local client = vim.lsp.get_client_by_id(ev.data.client_id)
+                if not client or client.name ~= 'clangd' then
+                    return
+                end
+
+                local value = ev.data.params.value
+                if value.title == 'indexing' then
+                    clangd_index_percent = (value.kind ~= 'end') and value.percentage or nil
+                    lualine.refresh({ place = { 'statusline' } })
+                end
+            end,
+        })
+
+        local function clangd_indexing()
+            if clangd_index_percent == nil then
+                return ''
+            end
+            return string.format('clangd indexing: %d%%%%', clangd_index_percent)
+        end
+
         lualine.setup({
             options = {
                 icons_enabled = true,
@@ -27,7 +52,7 @@ return {
                 lualine_b = { 'branch', 'diff' },
                 lualine_c = { 'windows' },
                 lualine_x = { 'searchcount', 'selectioncount', "filetype" },
-                lualine_y = { 'diagnostics' },
+                lualine_y = { 'diagnostics', clangd_indexing },
                 lualine_z = { 'progress', { 'location' } }
             },
         })
