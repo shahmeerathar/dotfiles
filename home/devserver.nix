d988aa30f07f3ec227657f96873aa73d693698d1
Shahmeer Athar <shahmeer.athar@gmail.com>
2026-08-26 16:00:33 -0600

    devserver zsh with correct locale

--- home/devserver.nix
+++ home/devserver.nix
@@ -1,4 +1,15 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  zshWithLocale = pkgs.symlinkJoin {
+    name = "zsh-with-locale";
+    paths = [pkgs.zsh];
+    nativeBuildInputs = [pkgs.makeBinaryWrapper];
+    postBuild = ''
+      wrapProgram $out/bin/zsh \
+        --set LOCALE_ARCHIVE "${pkgs.glibcLocales}/lib/locale/locale-archive"
+    '';
+    meta.mainProgram = "zsh";
+  };
+in {
   home = {
     username = "shahmeera";
     homeDirectory = "/cb/home/shahmeera";
@@ -6,7 +17,10 @@
 
   programs.tmux.shell = "~/.nix-profile/bin/zsh";
 
-  programs.zsh.initContent = ''
-    source ~/ws/scripts/rc.sh
-  '';
+  programs.zsh = {
+    package = zshWithLocale;
+    initContent = ''
+      source ~/ws/scripts/rc.sh
+    '';
+  };
 }
